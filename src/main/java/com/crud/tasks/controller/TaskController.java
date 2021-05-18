458232e133312d0d3a8aa50d342dd992b51c0911
Daniel Zak <D4nielZ4k@gmail.com>
2021-05-18 20:46:38 +0200

    getByiD

--- src/main/java/com/crud/tasks/controller/TaskController.java
+++ src/main/java/com/crud/tasks/controller/TaskController.java
@@ -4,6 +4,8 @@ import com.crud.tasks.domain.Task;
 import com.crud.tasks.domain.TaskDto;
 import com.crud.tasks.mapper.TaskMapper;
 import com.crud.tasks.service.DbService;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,41 +16,40 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/v1/task")
+@RequiredArgsConstructor
 public class TaskController {
 
+    private final DbService service;
+    private final TaskMapper taskMapper;
 
-    private DbService service;
-    private TaskMapper taskMapper;
-
-    @RequestMapping(method = RequestMethod.GET,value = "getTasks")
-    public List<TaskDto> getTasks(){
+    @RequestMapping(method = RequestMethod.GET, value = "getTasks")
+    public List<TaskDto> getTasks() {
         List<Task> tasks = service.getAllTasks();
         return taskMapper.mapToTaskDtoList(tasks);
-    }
 
+//
+ //  @RequestMapping(method = RequestMethod.GET, value = "getTaskss")
+ //  public List<TaskDto> getTaskss() {
+ //      return new ArrayList<>();
+ //  }
+//
+ //  @RequestMapping(method = RequestMethod.GET, value = "getTask")
+ //  public TaskDto getTask(Long taskId) {
+ //      return new TaskDto(1L, "test title", "test_content");
+ //  }
+//
+ //  @RequestMapping(method = RequestMethod.DELETE, value = "deleteTask")
+ //  public void deleteTask(Long taskId) {
+//
+ //  }
+//
+ //  @RequestMapping(method = RequestMethod.PUT, value = "updateTask")
+ //  public TaskDto updateTask(TaskDto taskDto) {
+ //      return new TaskDto(1L, "Edited test title", "Test content");
+ //  }
+//
+ //  @RequestMapping(method = RequestMethod.POST, value = "createTask")
+ //  public void createTask(TaskDto taskDto) {
 
-  // @RequestMapping(method = RequestMethod.GET, value = "getTasks")
-  // public List<TaskDto> getTasks() {
-  //     return new ArrayList<>();
-  // }
-
-  // @RequestMapping(method = RequestMethod.GET, value = "getTask")
-  // public TaskDto getTask(Long taskId) {
-  //     return new TaskDto(1L, "test title", "test_content");
-  // }
-
-  // @RequestMapping(method = RequestMethod.DELETE, value = "deleteTask")
-  // public void deleteTask(Long taskId) {
-
-  // }
-
-  // @RequestMapping(method = RequestMethod.PUT, value = "updateTask")
-  // public TaskDto updateTask(TaskDto taskDto) {
-  //     return new TaskDto(1L, "Edited test title", "Test content");
-  // }
-
-  // @RequestMapping(method = RequestMethod.POST, value = "createTask")
-  // public void createTask(TaskDto taskDto) {
-
-  // }
+   }
 }
